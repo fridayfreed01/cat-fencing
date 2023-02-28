33a420c23dd1543bf1672566384bb6c1dc55218a
Seth Freitag <friday.freed01@gmail.com>
2023-02-28 14:10:34 -0600

    recommit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+	public List<Card> deck;
+	public TextMeshProUGUI deckSizeText;
+
+	public Transform[] cardSlots;
+	public bool[] availableCardSlots;
+
+	public List<Card> discardPile;
+	public TextMeshProUGUI discardPileSizeText;
+
+	public Transform playerActiveCard;
+	public Transform enemyActiveCard;
+
+	private void Start()
+	{
+		for (int i = 0; i < cardSlots.Length; i++)
+        {
+			DrawCard();
+        }
+	}
+
+	public void DrawCard()
+	{
+		if (deck.Count >= 1)
+		{
+			Card randomCard = deck[Random.Range(0, deck.Count)];
+			for (int i = 0; i < availableCardSlots.Length; i++)
+			{
+				if (availableCardSlots[i] == true)
+				{
+					randomCard.gameObject.SetActive(true);
+					randomCard.handIndex = i;
+					randomCard.transform.position = cardSlots[i].position;
+					randomCard.hasBeenPlayed = false;
+					deck.Remove(randomCard);
+					availableCardSlots[i] = false;
+					return;
+				}
+			}
+		}
+	}
+
+	public void Shuffle()
+	{
+		if (discardPile.Count >= 1)
+		{
+			foreach (Card card in discardPile)
+			{
+				deck.Add(card);
+			}
+			discardPile.Clear();
+		}
+	}
+
+	private void Update()
+	{
+		
+	}
+
+}
