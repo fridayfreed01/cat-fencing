aaac6f646aec54abbb214daa1524da9792f5bd91
Seth Freitag <friday.freed01@gmail.com>
2023-02-28 12:48:43 -0600

    Cards are now 'playable,' but need to implement a FSM to command the flow of the battle mechanics

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public bool hasBeenPlayed;
+	public int handIndex;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	GameManager gm;
+
+	public GameObject effect;
+	public GameObject hollowCircle;
+
+	private void Start()
+	{
+		gm = FindObjectOfType<GameManager>();
+	}
+	private void OnMouseDown()
+	{
+		if (!hasBeenPlayed)
+		{
+			//Instantiate(hollowCircle, transform.position, Quaternion.identity);
+			transform.position = gm.playerActiveCard.position;
+			hasBeenPlayed = true;
+			gm.availableCardSlots[handIndex] = true;
+
+			
+		}
+	}
+
+	//void MoveToDiscardPile()
+	//{
+	//	Instantiate(effect, transform.position, Quaternion.identity);
+	//	gm.discardPile.Add(this);
+	//	gameObject.SetActive(false);
+	//}
+
+
+
 }
