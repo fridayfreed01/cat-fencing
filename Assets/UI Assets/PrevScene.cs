a27dced9dbde9951f458299a5b7bbda0dfe3070b
unawks02 <sarah.grace.f7@gmail.com>
2023-03-09 22:53:51 -0600

    Adding UI navigation button functionality

---
+++ Assets/UI Assets/PrevScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrevScene : MonoBehaviour
+{
+
+    private List<string> sceneHistory = new List<string>(); //list of scenes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    DontDestroyOnLoad(this.gameObject);  //Allow this object to persist between scene changes
+    sceneHistory.Add("GameStart");
+    }
+
+    public void addScene(string sceneName) {
+    sceneHistory.Add(sceneName);
+    }
+
+    public string PreviousScene() //this method was adapted from one found online
+    {
+            sceneHistory.RemoveAt(sceneHistory.Count -1);
+            string toReturn = (sceneHistory[sceneHistory.Count -1]);
+        return toReturn;
+    }
+    
+}
+  
