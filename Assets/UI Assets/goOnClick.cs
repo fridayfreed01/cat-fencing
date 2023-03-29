ff7372e7bcc2d26e5c3c62b84580475ef038e3de
unawks02 <sarah.grace.f7@gmail.com>
2023-03-29 13:08:05 -0500

    adding script to cat to follow cursor clicks

--- Assets/UI Assets/goOnClick.cs
+++ Assets/UI Assets/goOnClick.cs
@@ -6,46 +6,40 @@ public class goOnClick : MonoBehaviour
 {
     public Transform toFollow;
     public float speed;
+    private Vector3 targetPosition;
+    private float startingX;
 
     void Start()
     {
+        targetPosition = toFollow.position;
+        startingX = toFollow.position.x;
     }
 
     void Update()
     {
         //cat on right side of screen
-        //always update y and z values
-        float oldX = toFollow.position.x;
-        toFollow.position.Set(toFollow.position.x, Input.mousePosition.y, Input.mousePosition.z);
 
-        if (Input.GetMouseButtonDown(0)) {
-            //have cat move left to mouse then right
-            toFollow.transform.localScale = new Vector3(1, 1, 1); //move left
-            while (toFollow.position.x != Input.mousePosition.x) {
-                float newX = toFollow.position.x - speed;
-                Debug.Log("moving left");
-                if (newX >= Input.mousePosition.x) {
-                    toFollow.position.Set(newX, toFollow.position.y, toFollow.position.x);
-
-                }
-                else { 
-                    toFollow.position.Set(Input.mousePosition.x, toFollow.position.y, toFollow.position.x); 
-                }
-            }
-
-            toFollow.transform.localScale = new Vector3(-1, 1, 1); //move right
-            while (toFollow.position.x != oldX) {
-                float newX = toFollow.position.x + speed;
-                Debug.Log("moving right");
-                if (newX <= oldX) {
-                    toFollow.position.Set(newX, toFollow.position.y, toFollow.position.x);
-
-                }
-                else { toFollow.position.Set(oldX, toFollow.position.y, toFollow.position.x);
-                }
-            }
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            toFollow.transform.localScale = new Vector3(-1, 1, 1);
+            targetPosition = Input.mousePosition;
+            Debug.Log("moving to mouse");
         }
+        else
+        {
+            //always update y and z values
+            if (Mathf.Approximately(toFollow.position.x, startingX))
+            {
+                toFollow.transform.localScale = new Vector3(-1, 1, 1);
+                targetPosition = new Vector3(startingX, Input.mousePosition.y, Input.mousePosition.z);
+            }
+            else if (Mathf.Approximately(toFollow.position.x, targetPosition.x))
+            {
+                toFollow.transform.localScale = new Vector3(1, 1, 1);
+                targetPosition = new Vector3(startingX, Input.mousePosition.y, Input.mousePosition.z);
+            }
+        }
+        toFollow.position = Vector3.MoveTowards(toFollow.position, targetPosition, 100* speed * Time.deltaTime);
     }
 
 }
