00a782da786de6a50d77539623f2558780e36955
Seth Freitag <friday.freed01@gmail.com>
2023-04-26 14:21:12 -0500

    Push to main

--- Assets/Scripts/BattleDialogue.cs
+++ Assets/Scripts/BattleDialogue.cs
@@ -6,106 +6,82 @@ using TMPro;
 using Assets.Scripts;
 using System;
 
-public class NPCDialogue : MonoBehaviour
+namespace Assets.Scripts
 {
-    public GameObject dialogueBox;
-    public TextMeshProUGUI dialogueText;
-    public TextMeshProUGUI nameText;
-    public string[] dialogue;
-    public bool inRange;
-    public float textSpeed;
-    public bool dialogueActive;
-    public string NPC_name;
-    public BattleSystem battleSystem;
-    public Unit playerUnit;
-    public Unit enemyUnit;
-
-    private int index;
-
-    void Start()
+    public class BattleDialogue : MonoBehaviour
     {
+        public GameObject dialogueBox;
+        public TextMeshProUGUI dialogueText;
+        public TextMeshProUGUI nameText;
+        public string[] dialogue;
+        public bool inRange;
+        public float textSpeed;
+        public bool dialogueActive;
+        public string NPC_name;
+        public BattleSystem battleSystem;
+        public Unit playerUnit;
+        public Unit enemyUnit;
 
-    }
+        private int index;
 
-    void Update()
-    {
-        if (battleSystem.state == BattleState.PLAYER)
+        void Update()
         {
-            if (!dialogueActive)
+            if (battleSystem.state == BattleState.PLAYER)
             {
-                //StartDialogue();
-                Debug.Log("Dialogue");
-            }
-            else
-            {
-                if (dialogueText.text == dialogue[index])
+                if (!dialogueActive)
                 {
-                    NextLine();
+                    StartDialogue();
+                    Debug.Log("Dialogue");
                 }
                 else
                 {
-                    StopAllCoroutines();
-                    dialogueText.text = dialogue[index];
+                    if (dialogueText.text == dialogue[index])
+                    {
+                        NextLine();
+                    }
+                    else
+                    {
+                        StopAllCoroutines();
+                        dialogueText.text = dialogue[index];
+                    }
                 }
+
             }
-            
         }
-    }
 
-    IEnumerator TypeLine()
-    {
-        foreach (char c in dialogue[index].ToCharArray())
+        IEnumerator TypeLine()
         {
-            dialogueText.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            foreach (char c in dialogue[index].ToCharArray())
+            {
+                dialogueText.text += c;
+                yield return new WaitForSeconds(textSpeed);
+            }
         }
-    }
 
-    void NextLine()
-    {
-        if(index < dialogue.Length - 1)
+        void NextLine()
         {
-            index++;
-            dialogueText.text = string.Empty;
-            StartCoroutine(TypeLine());
+            if (index < dialogue.Length - 1)
+            {
+                index++;
+                dialogueText.text = string.Empty;
+                StartCoroutine(TypeLine());
+            }
+            else
+            {
+                dialogueBox.SetActive(false);
+                dialogueActive = false;
+                dialogueText.text = string.Empty;
+            }
         }
-        else
-        {
-            dialogueBox.SetActive(false);
-            dialogueActive = false;
-            dialogueText.text = string.Empty;
-            //player.canMove = true;
-            //player.GetComponent<Animator>().enabled = true;
-        }
-    }
 
-    void StartDialogue()
-    {
-        index = 0;
-        nameText.text = NPC_name;
-        dialogueActive = true;
-        dialogueBox.SetActive(true);
-        StartCoroutine(TypeLine());
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log(other.name);
-        if (other.CompareTag("Player"))
+        public void StartDialogue()
         {
-            inRange = true;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            inRange = false;
-            dialogueBox.SetActive(false);
-            dialogueActive = false;
-            //player.GetComponent<Animator>().enabled = true;
-            //player.canMove = true;
+            Debug.Log("dialogue starting");
+            //index = 0;
+            //nameText.text = NPC_name;
+            //dialogueActive = true;
+            //dialogueBox.SetActive(true);
+            //StartCoroutine(TypeLine());
         }
     }
 }
